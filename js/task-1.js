ce4c3dac1bcf857375076654b342c0b6bb5a17bc
5791-IM <142788119+5791-IM@users.noreply.github.com>
2024-01-11 22:08:23 +0200

    Summary and task-1.js

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const cotegoriesList = document.querySelector("ul#categories");
+
+const items = cotegoriesList.querySelectorAll("li.item");
+
+console.log(`Number of categories: ${items.length}`);
+
+items.forEach((item) => {
+  const title = item.querySelector("h2").textContent;
+  const subItems = item.querySelectorAll("li");
+
+  console.log(`Category: ${title}, \nElements: ${subItems.length}`);
+});
