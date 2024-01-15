524cdcc4a1a9755de94fa9c159bbb3e26b5b5e87
5791-IM <142788119+5791-IM@users.noreply.github.com>
2024-01-15 14:48:35 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,12 +1,12 @@
-const bodyElement = document.querySelector("body");
-const colorSpan = document.querySelector(".color");
-const changeColorButton = document.querySelector(".change-color");
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyElement = document.querySelector("body");
+const colorSpan = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
 
 changeColorButton.addEventListener("click", () => {
   const randomColor = getRandomHexColor();
